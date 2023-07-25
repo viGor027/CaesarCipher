10fb34445a4d77e148688244d5c6d084b3a8a2bd
viGor027 <vigor.wm@yahoo.com>
2023-07-25 10:49:13 +0200

    feature: database config

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from database import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///messages.db'
+
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
