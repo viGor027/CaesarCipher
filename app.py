cc9aec34ae55841db5ab9dafc7b9e0f79d9e13aa
viGor027 <vigor.wm@yahoo.com>
2023-07-25 17:38:32 +0200

    feature: json download

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import render_template, Blueprint, request
+from flask import render_template, Blueprint, request, send_file
 from CaesarCipher.logic.cipher import Cipher
+from CaesarCipher.logic.get_time import get_current_time
+import json
+
 
 
 index_blueprint = Blueprint('index', __name__)
@@ -12,10 +15,13 @@ messages = []
 def index():
     if request.method == "POST":
         message = request.form['message']
-        shift = int(request.form['shift'])
+        try:
+            shift = int(request.form['shift'])
+        except:
+            return render_template("index.html")
+        time = get_current_time()
         message_wrapped = Cipher(message, shift)
-        messages.append((message_wrapped.encrypted_message, shift))
-        print(messages, message)
+        messages.append((message_wrapped.encrypted_message, shift, time))
         return render_template("index.html")
     else:
         return render_template("index.html")
@@ -24,3 +30,14 @@ def index():
 @index_blueprint.route('/history')
 def history():
     return render_template("history.html", messages=messages)
+
+
+@index_blueprint.route('/download')
+def download():
+    data_json = {}
+    for msg, shift, time in messages:
+        print(msg, shift, time)
+        data_json[time] = [msg, shift]
+    with open('/history.json', 'w') as outfile:
+        json.dump(data_json, outfile)
+    return send_file('/history.json', as_attachment=True)
